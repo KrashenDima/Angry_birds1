4a2c02b89e302952adfc6183248b4eba499789d4
Крашенинников Дмитрий Евгеньевич <krasheninnikovdima30@gmail.com>
2022-04-11 14:09:31 +0500

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WMPLib;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            player.URL = "angry_birds_music.mp3";
+        }
+
+        private void play_music(object sender, RoutedEventArgs e)
+        {
+            player.controls.play();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 win = new Window1();
+            win.Show();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
